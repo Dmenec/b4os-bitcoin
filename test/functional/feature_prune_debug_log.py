4880c5043df1255e0545c2ded5477f8097196db1
Dmenec <Domenec.Madrid@uab.cat>
2026-02-24 15:53:35 +0100

    tests: feature_prune_debug_log

---
+++ test/functional/feature_prune_debug_log.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-present The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""An example functional test
+
+The module-level docstring should include a high-level description of
+what the test is doing. It's the first thing people see when they open
+the file and should give the reader information about *what* the test
+is testing and *how* it's being tested
+"""
+
+from test_framework.test_framework import BitcoinTestFramework
+
+class ExampleTest(BitcoinTestFramework):
+    # Each functional test is a subclass of the BitcoinTestFramework class.
+
+    # Override the set_test_params(), skip_test_if_missing_module(), add_options(), setup_chain(), setup_network()
+    # and setup_nodes() methods to customize the test setup as required.
+
+    def set_test_params(self):
+        """Override test parameters for your individual test.
+
+        This method must be overridden and num_nodes must be explicitly set."""
+        self.num_nodes = 1
+
+    def run_test(self):
+        """Main test logic"""
+
+        with self.nodes[0].assert_debug_log([], unexpected_msgs=['Prune configured']):
+            self.restart_node(0)
+
+        with self.nodes[0].assert_debug_log(['Prune configured']):
+            self.restart_node(0, extra_args=["-prune=550"])
+
+        par = self.nodes[0].getblockchaininfo().get('pruned')
+        assert par
+
+if __name__ == '__main__':
+    ExampleTest(__file__).main()
--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -101,6 +101,7 @@ BASE_SCRIPTS = [
     # Scripts that are run by default.
     # Longest test should go first, to favor running tests in parallel
     # vv Tests less than 5m vv
+    'feature_prune_debug_log.py',
     'wallet_rpc_basics.py',
     'feature_hello_world.py',
     'feature_fee_estimation.py',
